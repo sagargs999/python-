522815876a07fe3f13ad562456c53c9f42a83c3f
Sagar <gs2112006@gmail.com>
2026-09-04 20:24:43 +0530

    Implement getRow method for Pascal's Triangle

---
+++ LC_p119.py
@@ -0,0 +1,13 @@
+class Solution:
+    def getRow(self, rowIndex: int) -> list[int]:
+        triangle = []
+
+        for i in range(rowIndex + 1):
+            row = [1] * (i + 1)
+
+            for j in range(1, i):
+                row[j] = triangle[i - 1][j - 1] + triangle[i - 1][j]
+
+            triangle.append(row)
+
+        return triangle[rowIndex]
